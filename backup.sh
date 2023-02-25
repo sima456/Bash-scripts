bb9c51864be455f82e53d6f8558579575d942af3
Ishimwe fabrice <110179513+sima456@users.noreply.github.com>
2023-02-25 10:20:39 +0200

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+backup_dirs=("/etc" "/home" "/boot")
+dest_dir="/backup"
+dest_server="server1"
+backup_date=$(date +%b-%d-%y)
+
+echo "Starting backup of: ${backup_dirs[@]}"
+
+for i in "${backup_dirs[@]}"; do
+sudo tar -Pczf /tmp/$i-$backup_date.tar.gz $i
+if [ $? -eq 0 ]; then
+echo "$i backup succeeded."
+else
+echo "$i backup failed."
+fi
+scp /tmp/$i-$backup_date.tar.gz $dest_server:$dest_dir
+if [ $? -eq 0 ]; then
+echo "$i transfer succeeded."
+else
+echo "$i transfer failed."
+fi
+done
+
+sudo rm /tmp/*.gzecho "Backup is done."
